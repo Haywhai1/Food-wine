c636e418031a71600aa451ba4e8646fcdd00a610
Olawale Ayobami <148712150+haywhai@users.noreply.github.com>
2025-03-31 14:24:20 +0100

    hosted backend

--- frontend/src/shared/client.js
+++ frontend/src/shared/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: process.env.NODE_ENV === "production"
-    ? "https://food-wine.onrender.com"
+    ? "https://food-wine.onrender.com/api/v1"
     : "http://localhost:4040/api/v1",
    
     // headers: {
